740b8718526d459e9206c0180c9ddde10b4f8d00
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-10-22 12:36:27 -0400

    Update Centos_10.sh

--- Centos_10.sh
+++ Centos_10.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum update 
 sudo yum upgrade
-sudo nproc 
-sudo top 
-sudo lsblk 
+sudo nproc  # check the number of cpu in your system 
+sudo top  # used to check the cpu utilization percentage in your system 
+sudo lsblk  # used to check the size of your disk 
 sudo useradd kenyan-devops-group  
 sudo useradd Uganda
